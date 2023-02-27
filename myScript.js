b0487161f0e1bc9040c05aec244caae496bd91cd
Clarissa Banez <124168387+ClarissaBanez@users.noreply.github.com>
2023-02-27 13:28:04 +0100

    Define function for random comp selection

--- myScript.js
+++ myScript.js
@@ -1,3 +1,11 @@
-let playerSelection = (prompt("Rock, Paper or Scissor?")).toLowerCase();
-let computerSelection = getComputerChoice()
+let playerSelection = (prompt("Rock, Paper or Scissors?")).toLowerCase();
+let computerSelection = getComputerChoice();
 
+
+function getComputerChoice(){
+    let choice = ["rock", "paper", "scissors"];
+    let i = Math.floor(Math.random()*3);
+    return choice[i];
+}
+console.log(playerSelection);
+console.log(computerSelection);
