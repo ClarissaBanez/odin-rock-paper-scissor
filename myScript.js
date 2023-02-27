27c0a6c88aceb47b4d6d8fd78a15d42b5e543771
Clarissa Banez <124168387+ClarissaBanez@users.noreply.github.com>
2023-02-27 16:19:56 +0100

    Move prompt inside the playRound function

--- myScript.js
+++ myScript.js
@@ -1,7 +1,9 @@
 //get input from user and make it case-insensitive
 let playerSelection = (prompt("Rock, Paper or Scissors?")).toUpperCase();
 let computerSelection = getComputerChoice();
-
+let result = ""
+let playerScore = 0
+let computerScore = 0
 //generate randow choice for computer
 function getComputerChoice(){
     let choice = ["ROCK", "PAPER", "SCISSORS"];
@@ -13,26 +15,36 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     if (playerSelection === computerSelection){
         console.log("Draw.");
-        return "draw";
+        return result = "draw";
     } 
     else if (
         (playerSelection === "ROCK"  && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK")){
             console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-            return "win";
+            return result = "win";
         }
     else if (
         (playerSelection === "SCISSORS" && computerSelection ==="PAPER")
     ){
         console.log(`You win! ${playerSelection} beat ${computerSelection}.`); //to make the it grammatically correct
-        return "win";
+        return result = "win";
     }
     console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-    return "lose";
+    return result = "lose";
 }
-console.log(playerSelection, computerSelection);
-playRound(playerSelection,computerSelection);
 
 //5 round-game mechanic
 //make variables for playerScore and computerScore
 //max round is 5
 //if player reaches 3, winner.
+function game(){
+    for (let round = 1; round < 6; round++){
+
+        if  (result === "win") {
+            playerScore++;             
+        }
+        else if (result === "lose") {
+            computerScore++;
+        }
+        console.log(`Round ${round}, Player: ${playerScore}, Computer ${computerScore}`)
+    }
+}
