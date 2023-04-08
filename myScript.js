6b7afabd884a2bcfa3f68aaead626919774207ad
Clarissa Banez <124168387+ClarissaBanez@users.noreply.github.com>
2023-04-08 13:54:40 +0200

    Fix event listeners and logic for player input

--- myScript.js
+++ myScript.js
@@ -3,70 +3,66 @@ function getComputerChoice() {
     let i = Math.floor(Math.random() * 3);
     return choice[i];
 }
-const rock = document.querySelector('rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
 
-console.log(rock);
+rock.addEventListener('click', getPlayerChoice);
+paper.addEventListener('click', getPlayerChoice);
+scissors.addEventListener('click', getPlayerChoice);
 
-// rock.addEventListener('click', getPlayerChoice(e));
-// paper.addEventListener('click', getPlayerChoice(e));
-// paper.addEventListener('click', getPlayerChoice(e));
+function getPlayerChoice(e) {    
+    console.log(this.id);
+    return this.id;
+}
 
+//set up logic for a round
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        console.log("Draw.");
+        return "draw";
+    }
+    else if (
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK")) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+        return "win";
+    }
+    else if (
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+    ) {
+        console.log(`You win! ${playerSelection} beat ${computerSelection}.`); //to make the it grammatically correct
+        return "win";
+    }
+    else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        return "lose";
+    }
+}
 
-// function getPlayerChoice() {    
-//     console.log(this);
-// }
-// //set up logic for a round
-// function playRound(playerSelection, computerSelection) {
-//     if (playerSelection === computerSelection) {
-//         console.log("Draw.");
-//         return "draw";
-//     }
-//     else if (
-//         (playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "PAPER" && computerSelection === "ROCK")) {
-//         console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-//         return "win";
-//     }
-//     else if (
-//         (playerSelection === "SCISSORS" && computerSelection === "PAPER")
-//     ) {
-//         console.log(`You win! ${playerSelection} beat ${computerSelection}.`); //to make the it grammatically correct
-//         return "win";
-//     }
-//     else {
-//         console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-//         return "lose";
-//     }
-// }
+function getWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        console.log("You won the game!");
+    }
+    else if (playerScore < computerScore) {
+        console.log("You lost the game!");
+    }
+    else
+        console.log("It's a draw!")
+}
 
-// function getWinner(playerScore, computerScore) {
-//     if (playerScore > computerScore) {
-//         console.log("You won the game!");
-//     }
-//     else if (playerScore < computerScore) {
-//         console.log("You lost the game!");
-//     }
-//     else
-//         console.log("It's a draw!")
-// }
-
-// function game() {
-//     let playerScore = 0
-//     let computerScore = 0
-//     for (let round = 1; round < 6; round++) {
-//         let playerSelection = getPlayerChoice();
-//         let computerSelection = getComputerChoice(); //generate random choice for computer
-//         console.log(`Round ${round}, Player: ${playerSelection}, Computer ${computerSelection}`)
-//         let result = playRound(playerSelection, computerSelection);
-//         if (result === "win") {
-//             playerScore++;
-//         }
-//         else if (result === "lose") {
-//             computerScore++;
-//         }
-//         console.log(`Score: ${playerScore}-${computerScore}`)
-//     }
-//     getWinner(playerScore, computerScore);
-// }
-// game();
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    for (let round = 1; round < 6; round++) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice(); //generate random choice for computer
+        console.log(`Round ${round}, Player: ${playerSelection}, Computer ${computerSelection}`)
+        let result = playRound(playerSelection, computerSelection);
+        if (result === "win") {
+            playerScore++;
+        }
+        else if (result === "lose") {
+            computerScore++;
+        }
+        console.log(`Score: ${playerScore}-${computerScore}`)
+    }
+    getWinner(playerScore, computerScore);
+}
+game();
