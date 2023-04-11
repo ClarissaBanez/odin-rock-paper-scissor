d9a99a4f61ea28e12a3eb1c10dee96bb17cf8029
Clarissa Banez <124168387+ClarissaBanez@users.noreply.github.com>
2023-04-11 10:43:58 +0200

    Refactor for readability

--- myScript.js
+++ myScript.js
@@ -1,34 +1,32 @@
 let playerScore = 0;
 let computerScore = 0;
 let round = 0;
-const buttons = document.querySelectorAll('.playerInput');
-buttons.forEach(button => button.addEventListener('click', playRound))
-resetButton.addEventListener('click', gameReset)
-//set up logic for a round
+const playerInput = document.querySelectorAll('.playerInput');
+playerInput.forEach(button => button.addEventListener('click', playRound))
+
+
 function playRound(e) {
     round ++;
     let playerSelection = e.target.id;
     let computerSelection = getComputerChoice()
-    result = getResult(playerSelection, computerSelection);
+    let result = getResult(playerSelection, computerSelection);
     scoreUpdate(result);
     displayResults(playerSelection, computerSelection, resultString);
     checkWinner(playerScore, computerScore);
-
-    // if checkwinner 
 }
 
 function displayResults(playerSelection, computerSelection, resultString){
-    playerChoice = document.getElementById('playerChoice');
-    computerChoice = document.getElementById('computerChoice');
-    roundResult = document.getElementById('roundResult');
+    let playerChoice = document.getElementById('playerChoice');
+    let computerChoice = document.getElementById('computerChoice');
+    let roundResult = document.getElementById('roundResult');
 
     playerChoice.innerText = `Player chooses ${playerSelection}.`;
     computerChoice.innerText = `Computer chooses ${computerSelection}.`;
     roundResult.innerText += `Round ${round}: ${resultString}\n`;
 }
 function scoreUpdate(result){
-    playerScoreBoard = document.getElementById('playerScore');
-    computerScoreBoard = document.getElementById('computerScore');
+    let playerScoreBoard = document.getElementById('playerScore');
+    let computerScoreBoard = document.getElementById('computerScore');
     if (result === "win"){
         playerScore++;
     }
@@ -63,31 +61,30 @@ function getResult(playerSelection, computerSelection) {
 }
 
 function checkWinner(playerScore, computerScore) {
-    gameResult = document.getElementById('gameResult');
-    resetButton = document.getElementById('resetButton');
     if (playerScore < computerScore && computerScore === 5) {
         gameOver("You lost the game!")
-        
     }
     else if (playerScore > computerScore && playerScore === 5){
         gameOver("You won the game!");
     }
-    return false;
+    return;
 }
 
 function gameOver(gameOverMessage){
-    gameResult.innerText = gameOverMessage;
+    let gameResult = document.getElementById('gameResult');
+    let resetButton = document.getElementById('resetButton');
+    resetButton.addEventListener('click', gameReset)
     resetButton.style.display = "block";
+    gameResult.innerText = gameOverMessage;
     playerChoice.innerText = "";
     computerChoice.innerText = "";
-    buttons.forEach(button => button.style.display="none");
+    playerInput.forEach(button => button.style.display="none");//removes the play input buttons
 }
 function gameReset(){
     playerScore = 0;
     computerScore = 0;
     round = 0;
-    playerScoreBoard.innerText=`${playerScore}`;
-    computerScoreBoard.innerText=`${computerScore}`;
+    scoreUpdate();
     playerChoice.innerText = "";
     computerChoice.innerText = "";
     roundResult.innerText= "";
